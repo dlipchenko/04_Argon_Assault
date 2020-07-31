083548811b3e0a580787f5a7fef2315e3a1d4826
Dmytro <dmytro.lipchenko@gmail.com>
2020-07-31 00:39:15 -0400

    added terrain, player, splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 4f);
+    }
+
+    private void Awake()
+    {
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
